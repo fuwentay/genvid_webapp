94243c87a4ebe2828f6cec5fb16129f56ac3f50f
fuwentay <fuwentay@gmail.com>
2023-09-27 13:37:51 +0800

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';  // Adjust the path if your App component is in a different location
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
